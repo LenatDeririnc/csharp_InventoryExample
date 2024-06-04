eeccfd3116d697056253e7671b35602b7f7e8a7a
Lenat_1 <smillerlenatderirinc@gmail.com>
2024-06-04 14:48:16 +0300

    remove unused properties

--- InventoryTest/InventoryObjects/BaseInventoryObject.cs
+++ InventoryTest/InventoryObjects/BaseInventoryObject.cs
@@ -4,8 +4,6 @@ public abstract class BaseInventoryObject
 {
     public abstract string Name { get; }
     public abstract int MaxContainerCount { get; }
-    
-    public Action<int> OnCountChange { get; set; }
 
     private int m_count = 0;
     
@@ -22,13 +20,7 @@ public abstract class BaseInventoryObject
                 Console.WriteLine($"[WARNING] inventory object \"{Name}\" can't be less than 0 elements");
             }
             
-            int prevValue = m_count;
-            
             m_count = Math.Clamp(value, 0, MaxContainerCount);
-
-            if (prevValue != m_count) {
-                OnCountChange?.Invoke(m_count);
-            }
         }
     }
 
